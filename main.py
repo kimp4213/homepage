0f779ed09b319614d2bd92b161bc98f14f5a8677
kimp4213 <109699521+kimp4213@users.noreply.github.com>
2022-07-20 23:18:43 +0200

    Added initial homepage

---
+++ main.py
@@ -0,0 +1,54 @@
+#  350 how websites work
+
+# websites consist of 3 types of files
+
+# html are responsible for the structure (add image, add buttons and so on)
+# css are used for styling/appearence the website
+# javascript gives the behaviour
+
+# 352 + 353 HTML = hypertext markup language
+
+# we can use https://codepen.io --> start coding --> change view --> third
+
+# Let us enter
+# The adventures of Sherlock Holmes
+# by
+# Sir Arthur Conan Doyle
+
+# html consist of two parts: <HTML element HTML attribute>
+# First is what we want and second is how we want to modify it
+# comments are made using <!--COMMENT -->
+
+# We can change the heading using <h1> in front. Also remember we have to end it using </h1>. These are called
+# These are called start and end tag
+# We can make Sir Arthur Conan Doyle smaller than the title using h2 and h2 for by
+# The headings only goes to h6
+# Creating space can be done using a line break <br>. We do not have to close these (=called self-closing tags)
+# we can create horizontal lines above and below our text with a given size using <hr size="3"> (self-closing)
+# lastly we can center it at the beginning and end using <center>
+
+# So our code becomes
+# <center>
+# <hr size="3">
+# <h1> The adventures of Sherlock Holmes</h1>
+# <br>
+# <h3>by</h3>
+# <br>
+# <h2> Sir Arthur Conan Doyle</h2>
+# We can check documentations using devdocs.io
+# <hr size="3">
+# </center>
+
+# 355 HTML boilerplate. We will continue in Personal Site.html.
+# Here we get a template called a boilerplate
+
+### 359 anchor tags ##
+
+# To make a like reference, we use the anchor tag
+# <a href = "LINK DISTINATION" > Link text </a>
+# The link is blue if we haven't clicked on it and purple if we've already clicked it.
+# To create a separate page, we can inside the html folder we can right click and create a new file
+# we then create a new anchor tag and the href will be the ref to this new file
+
+# 368 publish webpage on the internet. For this we use GitHub
+# we use kimp4213@gmail.com and password kimphan95
